e99a75f151f98e435d75c83d7e629965fdc3ac13
Saurav Aggarwal <sauravaggarwal98@gmail.com>
2020-01-12 18:47:32 +0530

    working correctly now

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,HostListener} from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
+  show : boolean = true;
+  isDisabled : boolean = false;
+  heading = "ENCRYPTO";
+  url="";
+  logo = require('../images/logo.png');
+  arrow = require('../images/arrow.png');
+  arrow_down = require('../images/arrow_alt.png');
   constructor() { }
 
   ngOnInit() {
+    if (window.screen.width <= 600) { // 768px portrait
+      this.show = false;
+      this.isDisabled = false;
+      this.url=this.arrow;
+
+    }
+    else{
+      this.show = true;
+      this.isDisabled = true;
+      this.url=this.logo;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  test(){
+  if (window.screen.width <= 600) { // 768px portrait
+    this.show = false;
+    this.isDisabled = false;
+    this.url=this.arrow;
+
+  }
+  else{
+    this.show = true;
+    this.isDisabled = true;
+    this.url=this.logo;
+  }
+  }
+ 
+ 
+
+  toggle()
+  {
+    if(!this.isDisabled){
+    this.show=!this.show;
+    if(this.show)
+      this.url = this.arrow_down;
+    else
+    this.url = this.arrow; 
+  }
+  
+
   }
 
 }
